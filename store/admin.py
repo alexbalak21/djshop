9306a492c67cd81e45462bdadda1432a137f62c6
alexbalak21 <alex.balak@outlook.com>
2021-03-13 12:18:43 +0100

    admin

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Category, Product
+from .models import Category, Product
 
 
 @admin.register(Category)
@@ -9,5 +9,9 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 @admin.register(Product)
-class ProdictAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'slug', 'price']
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'slug', 'price',
+                    'in_stock', 'created', 'updated']
+    list_filter = ['in_stock', 'is_active']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('title',)}
